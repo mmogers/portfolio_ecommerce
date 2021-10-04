b5e49df37ab6c37eab773d82bc360750b80f53b4
Marina Moger <marina.moger@gmail.com>
2021-10-04 21:09:52 +0300

    Initial commit

---
+++ src/lesson1/helloWorld/HelloWorld.java
@@ -0,0 +1,11 @@
+package lesson1.helloWorld;
+
+import lesson1.helloWorldLibrary.WorldLibrary;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java 2 course" + WorldLibrary.getName());
+        System.out.println("I libe ");
+    }
+}
