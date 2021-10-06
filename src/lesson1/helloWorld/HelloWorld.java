23f20eaf7be2850016fecbb7945c4cf977aeb9ad
Marina Moger <marina.moger@gmail.com>
2021-10-06 21:27:45 +0300

    lesson 2 - comments rules

--- src/lesson1/helloWorld/HelloWorld.java
+++ src/lesson1/helloWorld/HelloWorld.java
@@ -5,7 +5,14 @@ import lesson1.helloWorldLibrary.WorldLibrary;
 public class HelloWorld {
 
     public static void main(String[] args) {
+
+        WorldLibrary theWorld = new WorldLibrary();
+        theWorld.setPlanetName("Earth");
+
         System.out.println("Welcome to Java 2 course" + WorldLibrary.getName());
-        System.out.println("I libe ");
+        System.out.println("I live on a planet named " + theWorld.getPlanetName());
+
+        theWorld.setPlanetName("Mars");
+        System.out.println("Now I want to go on " + theWorld.getPlanetName());
     }
 }
---
+++ src/lesson2/syntax/Fundamentals.java
@@ -0,0 +1,33 @@
+package lesson2.syntax;
+
+public class Fundamentals {
+    public static void main(String[] args) {
+
+//COMMENTS
+        //We will write simple one line comments here
+
+        /*
+        line 1 of the comment
+        line 2 of the comment
+        line 3
+        ...
+         */
+    }
+
+
+    /**
+     * someMethod should be used in following cases - when the method should be used
+     * @param param1 - name of the user
+     * @param param2 -
+     * @return
+     */
+    public String someMethod (String param1, int param2){
+        return "something";
+    }
+
+}
+
+/*-
+keeps the original formatting , even if we beautify the code , stays the same , unformatted
+sdagfaksgf sakjgafkswjdf asekhfraksjefh kjfhsekaj.sdhgkfe.j iyeswalkrfha.ksehf liyea;lfrhyaselkwhfd lkeaywrflawherlkaws lhyaewsflkhasedf seaziltahlkwesrf eslk/ahlwshef
+ */
