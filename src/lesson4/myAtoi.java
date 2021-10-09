e0aae2c08df3b1641ea23246d434f13096c780c4
Marina Moger <marina.moger@gmail.com>
2021-10-09 22:57:16 +0300

    lesson 4 - homework - myAtoi

--- src/lesson4/myAtoi.java
+++ src/lesson4/myAtoi.java
@@ -3,30 +3,52 @@ package lesson4;
 public class myAtoi {
 
     public static void main(String[] args) {
-        String string = "   -256";
+        String string = "-91283472332";
+
+        myAtoi(string);
+    }
+
+
+    static void myAtoi(String s){
+        s = s.trim();
         int counter = 0;
-        for (int i = 0; i < string.length(); i++) {
-            if(string.charAt(i) == ' ');
-            counter ++;
-        }
         boolean isNegative = false;
-        if(string.charAt(counter) == '-'){ //detects +/- sets if the number is negative or positive
+        if(s.charAt(counter) == '-'){ //detects +/- sets if the number is negative or positive
             isNegative = true;
             counter++;
-        }else if (string.charAt(counter) == '+'){
+        }else if (s.charAt(counter) == '+'){
             isNegative = false;
             counter++;
         }
-        for (int i = counter; i <string.length() ; i++) { //eliminates zeroes
-            if(string.charAt(i) == '0'){
+        //System.out.println("counter " + counter + " " + isNegative);
+
+
+        for (int i = counter; i <s.length() ; i++) { //eliminates zeroes
+            if(s.charAt(i) == '0'){
                 counter++;
             }else {
                 break;
             }
         }
-        int power = 0;
-      // while()
+        //System.out.println("Zeroes" + counter);
+        long  number = 0; //to get the number by multiplying to 10 ND ADDING UNITS
+        while (((int)(s.charAt(counter)) <= 57) &&((int)(s.charAt(counter)) >= 48)){ //calculates the number, taking ascii of numbers 48-57
+            number = number * 10 + ((int)s.charAt(counter) - 48);
+            counter++; // the place in string
+            //System.out.println(number);
+            if (number > Math.pow(2,31)){ //checks if its in range 2 in pow 31
+                number = (long)Math.pow(2,31);
+                break;
+            }
+            if (counter >= s.length()){ //not to get out of memory
+                break;
+            }
+        }
 
-
+        if (isNegative){
+            System.out.println((0-number));
+        }else{
+            System.out.println(number);
+        }
     }
 }
