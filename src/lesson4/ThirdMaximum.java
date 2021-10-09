19eb52b1e9394365cf57a24b2e1208a431ed95fc
Marina Moger <marina.moger@gmail.com>
2021-10-09 14:06:29 +0300

    lesson 4 - The third Maximum was done - team work

---
+++ src/lesson4/ThirdMaximum.java
@@ -0,0 +1,60 @@
+package lesson4;
+
+import java.util.Scanner;
+
+public class ThirdMaximum {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+     /*   System.out.println("Please enter the length of array");
+        int arrayLength = scanner.nextInt();
+
+        int [] array = new int[arrayLength];
+        for (int i = 0; i < arrayLength; i++) {
+            System.out.println("please enter the number " + (i+1) + "to array");
+            array[i] = scanner.nextInt();
+        }
+
+        *//*for (int i = 0; i < arrayLength; i++) {
+            System.out.println(array[i]);
+        }*/
+
+        int [] array = {3};
+        int [] array = {3,2,1};
+
+        int max = array[0];
+        int min = array[0];
+
+        for (int item:array
+             ) {
+            if (item >= max){
+                max = item;
+            }
+            if (item <=min){
+                min = item;
+            }
+        }
+      // System.out.println(max + " " + min);
+        int secondMax = min;
+        for (int item:array
+             ) {
+            if(item> secondMax && item < max){
+                secondMax = item;
+            }
+        }
+
+
+        int thirdMaximum = min;
+        for (int item:array
+             ) {
+            if(item> thirdMaximum && item < secondMax){
+                thirdMaximum = item;
+            }
+        }
+
+      //  System.out.println(max + " " + secondMax + " " + thirdMaximum);
+        System.out.println( "The third maximum is: " + thirdMaximum);
+    }
+
+}
