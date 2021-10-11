4c59695579361da156c9d5b400f2a3c467a6e55b
Marina Moger <marina.moger@gmail.com>
2021-10-11 16:02:02 +0300

    myAtoi - optimized, but needs more 105 lines

--- src/lesson4/RomanNumerals.java
+++ src/lesson4/RomanNumerals.java
@@ -1,6 +1,5 @@
 package lesson4;
 
-import java.util.List;
 import java.util.Scanner;
 
 public class RomanNumerals {
@@ -11,114 +10,96 @@ public class RomanNumerals {
 
         System.out.println("Please enter the number.");
         int intNumber = scanner.nextInt();
-        while ((intNumber>3999 || intNumber<1)){ //wait for the input of int number in a range [1-3999]
+        while ((intNumber > 3999 || intNumber < 1)) { //wait for the input of int number in a range [1-3999]
             System.out.println("The number is out of range. Enter the number in the range [1-3999] ");
             intNumber = scanner.nextInt();
         }
 
-        int unitsDigit = 0; //the
-        int tensDigit = 0;
-        int hundredsDigit = 0;
-        int thousandsDigit = 0;
         int unitsGrade = 1;
         int tensGrade = 10;
         int hundredsGrade = 100;
         int thousandsGrade = 1000;
-if (intNumber < 10){
-    unitsDigit = intNumber % 10;
-    String numUnits = romanNumberCreator(unitsDigit,unitsGrade);
-    System.out.println(numUnits);
-}
-else if(intNumber < 100){
-    unitsDigit = intNumber % 10;
-    tensDigit = intNumber / 10;
-    String numUnits = romanNumberCreator(unitsDigit,unitsGrade);
-    String numTens = romanNumberCreator(tensDigit,tensGrade);
-    System.out.println(numTens + numUnits);
-}
-else if(intNumber < 1000){
-    unitsDigit = intNumber % 10;
-    tensDigit = (intNumber / 10) % 10;
-    hundredsDigit = intNumber / 100;
-    String numUnits = romanNumberCreator(unitsDigit,unitsGrade);
-    String numTens = romanNumberCreator(tensDigit,tensGrade);
-    String numHundreds = romanNumberCreator(hundredsDigit,hundredsGrade);
-    System.out.println(numHundreds + numTens + numUnits);
-}
-else {
-    unitsDigit = intNumber % 10;
-    tensDigit = (intNumber / 10) % 10;
-    hundredsDigit = (intNumber / 100) %10;
-    thousandsDigit = intNumber / 1000;
-    String numUnits = romanNumberCreator(unitsDigit,unitsGrade);
-    String numTens = romanNumberCreator(tensDigit,tensGrade);
-    String numHundreds = romanNumberCreator(hundredsDigit,hundredsGrade);
-    String numThousands = romanNumberCreator(thousandsDigit,thousandsGrade);
-    System.out.println(numThousands + numHundreds + numTens + numUnits);
-}
 
+        int unitsDigit = intNumber % 10;
+        int tensDigit = (intNumber / 10) % 10;
+        int hundredsDigit = (intNumber / 100) % 10;
+        int thousandsDigit = intNumber / 1000;
+
+        if (thousandsDigit != 0) {
+            String numThousands = romanDigitCreator(thousandsDigit, thousandsGrade);
+            System.out.print(numThousands);
+        }
+        if (hundredsDigit != 0) {
+            String numHundreds = romanDigitCreator(hundredsDigit, hundredsGrade);
+            System.out.print(numHundreds);
+        }
+        if (tensDigit != 0) {
+            String numTens = romanDigitCreator(tensDigit, tensGrade);
+            System.out.print(numTens);
+        }
+        if (unitsDigit != 0) {
+            String numUnits = romanDigitCreator(unitsDigit, unitsGrade);
+            System.out.print(numUnits);
+        }
     }
 
-    static String romanNumberCreator (int number, int grade){
+    static String romanDigitCreator(int number, int grade) {
         char a = ' ';
         char b = ' ';
         char c = ' ';
 
-        if(grade == 1) {
+        if (grade == 1) {
             a = 'I';
             b = 'V';
             c = 'X';
-        }else if (grade == 10){
+        } else if (grade == 10) {
             a = 'X';
             b = 'L';
             c = 'C';
-        }else if (grade == 100){
+        } else if (grade == 100) {
             a = 'C';
             b = 'D';
             c = 'M';
-        }else if (grade == 1000){
+        } else if (grade == 1000) {
             a = 'M';
             b = ' ';
             c = ' ';
 
         }
 
-        char [] romanNumber = {};
+        char[] romanDigit = {};
 
-switch (number){
-    case 1:
-        romanNumber = new char []{a};
-        break;
-
-    case 2:
-        romanNumber = new char [] {a,a};
-        break;
-    case 3:
-        romanNumber = new char [] {a, a, a};
-        break;
-    case 4:
-        romanNumber = new char [] {a,b};
-        break;
-    case 5:
-        romanNumber = new char [] {b};
-        break;
-    case 6:
-        romanNumber = new char [] {b, a};
-        break;
-    case 7:
-        romanNumber = new char [] {b, a, a};
-        break;
-    case 8:
-        romanNumber = new char [] {b, a, a, a};
-        break;
-    case 9:
-        romanNumber = new char [] {a,c};
-        break;
-    default:
-       break;
-}
-        String stringRomanNumber  = new String(romanNumber);
-       // System.out.println(stringRomanNumber);
-return stringRomanNumber;
+        switch (number) {
+            case 1:
+                romanDigit = new char[]{a};
+                break;
+            case 2:
+                romanDigit = new char[]{a, a};
+                break;
+            case 3:
+                romanDigit = new char[]{a, a, a};
+                break;
+            case 4:
+                romanDigit = new char[]{a, b};
+                break;
+            case 5:
+                romanDigit = new char[]{b};
+                break;
+            case 6:
+                romanDigit = new char[]{b, a};
+                break;
+            case 7:
+                romanDigit = new char[]{b, a, a};
+                break;
+            case 8:
+                romanDigit = new char[]{b, a, a, a};
+                break;
+            case 9:
+                romanDigit = new char[]{a, c};
+                break;
+            default:
+                break;
+        }
+        return String.valueOf(romanDigit);
     }
 }
